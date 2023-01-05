d1e8919828a08049fb7791f76baac3496aaa110e
sokelva <sokelva@mail.ru>
2023-01-05 15:56:12 +0300

    8.6. Итоги - ЗАДАНИЕ1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,58 @@ namespace Task1
 {
     class Program
     {
+        /// <summary>
+        /// Задание 1: Программа чистит нужную нам папку от файлов и папок, которые не использовались более 30 мину
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            bool paramsExist = args.Length > 0;
+            string path = paramsExist ? args[0] : Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\Task1";
+            
+            //Создаем папку, если ее нет
+            if (! Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"При создании папки возникла ошибка: {e.Message}");
+                }
+            }
+
+            string[] dirs = Directory.GetDirectories(path,"*.*", SearchOption.TopDirectoryOnly);// Проверяем только Task1
+
+            foreach (var item in dirs)
+            {
+                DirectoryInfo itemInfo = new DirectoryInfo(item);
+                Console.WriteLine($"{item}, {itemInfo.CreationTime}\n----------{itemInfo.CreationTime.AddMinutes(30)}");
+                
+                if (DateTime.Now > itemInfo.CreationTime.AddMinutes(30))
+                {
+                    if (Directory.Exists(item))
+                    {
+                        DeleteFile(itemInfo);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+   
+
+        public static void DeleteFile(DirectoryInfo itemInfo)
+        {
+            try
+            {
+                itemInfo.Delete(true);
+                Console.WriteLine($"Удалена папка: {itemInfo}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"При удалении папки {itemInfo} возникла ошибка: {e.Message}");
+            }
         }
     }
 }
